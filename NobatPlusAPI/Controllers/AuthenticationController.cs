93428cbf97204b404aa44932123ed3d6abeb60a2
mimtavoosi <tavoosi.insta20@gmail.com>
2024-07-19 15:26:39 +0330

    Api App First Configs

---
+++ NobatPlusAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NobatPlusAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NobatPlusAPI.Controllers
+{
+    [Route("authentication")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var key = _configuration["Jwt:Key"];
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("userId", user.UserId.ToString()),
+            new Claim("firstName", user.FirstName),
+            new Claim("lastName", user.LastName)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: signingCredentials);
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(tokenString);
+        }
+
+
+        private CityInfoUser ValidateUserCredentials(string? userName,
+            string? password)
+        {
+            return new CityInfoUser(1,userName??"","Mohammad","Tavoosi","Esfahan") ;
+        }
+    }
+}
