171f8e59483f9b7f0270ed802003b33d97cb0578
mimtavoosi <tavoosi.insta20@gmail.com>
2024-08-14 14:34:48 +0330

    0524_FirstCommit

---
+++ NobatPlusDATA/DataLayer/Repositories/IPersonRep.cs
@@ -0,0 +1,20 @@
+﻿using NobatPlusDATA.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobatPlusDATA.DataLayer.Repositories
+{
+    public interface IPersonRep
+    {
+        public List<Person> GetAllPersons(int pageIndex = 1,int pagesize = 20, string searchText ="");
+        public Person GetPersonById(int personId);
+        public void AddPerson(Person person);
+        public void EditPerson(Person person);
+        public void RemovePerson(Person person);
+        public void RemovePerson(int personId);
+        public bool ExistPerson(int personId);
+    }
+}
---
+++ NobatPlusDATA/DataLayer/Services/PersonRep.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NobatPlusDATA.DataLayer.Repositories;
+using NobatPlusDATA.Domain;
+using NobatPlusDATA.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobatPlusDATA.DataLayer.Services
+{
+    public class PersonRep : IPersonRep
+    {
+
+        private NobatPlusContext _context;
+        public PersonRep()
+        {
+            _context = DbTools.GetDbContext();
+        }
+
+        public void AddPerson(Person person)
+        {
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+            _context.Entry(person).State = EntityState.Detached;
+        }
+
+        public void EditPerson(Person person)
+        {
+            _context.Persons.Update(person);
+            _context.SaveChanges();
+            _context.Entry(person).State = EntityState.Detached;
+        }
+
+        public bool ExistPerson(int personId)
+        {
+            return _context.Persons.Any(x => x.ID == personId);
+        }
+
+        public List<Person> GetAllPersons(int pageIndex = 1, int pagesize = 20, string searchText = "")
+        {
+            return _context.Persons.Where(x => 
+            (!string.IsNullOrEmpty(x.FirstName.ToString()) && x.FirstName.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.LastName.ToString()) && x.LastName.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.NaCode.ToString()) && x.NaCode.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.PhoneNumber.ToString()) && x.PhoneNumber.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.Email.ToString()) && x.Email.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.Description.ToString()) && x.Description.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.DateOfBirth.ToString()) && x.DateOfBirth.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.CreateDate.ToString()) && x.CreateDate.ToString().Contains(searchText))
+           || (!string.IsNullOrEmpty(x.UpdateDate.ToString()) && x.UpdateDate.ToString().Contains(searchText))
+            ).OrderByDescending(x=> x.CreateDate).ToList().ToPaging(pageIndex,pagesize);
+        }
+
+        public Person GetPersonById(int personId)
+        {
+            return _context.Persons.Find(personId);
+        }
+
+        public void RemovePerson(Person person)
+        {
+            _context.Persons.Remove(person);
+            _context.SaveChanges();
+            _context.Entry(person).State = EntityState.Detached;
+        }
+
+        public void RemovePerson(int personId)
+        {
+            var person = GetPersonById(personId);
+            RemovePerson(person);
+        }
+    }
+}
