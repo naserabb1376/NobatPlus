583c7ff30ee89f67b5aa0daa27e203cc9ea2ecbf
mimtavoosi <tavoosi.insta20@gmail.com>
2024-09-04 15:56:51 +0330

    Enable Verifycode (1403/06/14)

---
+++ NobatPlusAPI/Tools/ConditionalRegularExpressionAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NobatPlusAPI.Tools
+{
+    public class ConditionalRegularExpressionAttribute : ValidationAttribute
+    {
+        private readonly string _comparisonProperty;
+        private readonly string _pattern;
+
+        public ConditionalRegularExpressionAttribute(string comparisonProperty, string pattern)
+        {
+            _comparisonProperty = comparisonProperty;
+            _pattern = pattern;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var property = validationContext.ObjectType.GetProperty(_comparisonProperty);
+            if (property == null)
+                throw new ArgumentException("Property with this name not found");
+
+            var comparisonValue = property.GetValue(validationContext.ObjectInstance);
+
+            if (comparisonValue is int intValue && intValue == 1)
+            {
+                if (value is string stringValue && !System.Text.RegularExpressions.Regex.IsMatch(stringValue, _pattern))
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
