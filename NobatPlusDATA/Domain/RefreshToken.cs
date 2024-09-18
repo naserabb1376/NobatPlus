9860a16f7fda04c0c32b8294ac3abdbc70f507b4
Naser <Naserabb76@outlook.com>
2024-09-18 12:45:12 +0330

    dsfsd

---
+++ NobatPlusDATA/Domain/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobatPlusDATA.Domain
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } // شناسه کاربر
+        public string Token { get; set; } // رفرش توکن
+        public DateTime ExpiryDate { get; set; } // تاریخ انقضا
+        public DateTime CreatedDate { get; set; } = DateTime.Now; // تاریخ ایجاد
+        public DateTime? RevokedDate { get; set; } // تاریخ لغو
+    }
+}
